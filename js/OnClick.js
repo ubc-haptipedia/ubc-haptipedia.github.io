d3be2958826714bab8e9a547da071b8eaf68032f
ubc-haptipedia <40783159+ubc-haptipedia@users.noreply.github.com>
2018-07-09 17:57:52 -0700

    Update OnClick.js

--- js/OnClick.js
+++ js/OnClick.js
@@ -238,9 +238,36 @@ $('.data').on("click", function(e){
 
 	else{
 
+		// Get the modal
+		  var modal = document.getElementById('myModal');
+
+		  // Get the button that opens the modal
+		  
+
+		  // Get the <span> element that closes the modal
+		  var span = document.getElementsByClassName("close")[0];
+
+		  // When the user clicks the button, open the modal 
+		 
+	      modal.style.display = "block";
+		  
+
+		  // When the user clicks on <span> (x), close the modal
+		  span.onclick = function() {
+		      modal.style.display = "none";
+		  }
+
+		  // When the user clicks anywhere outside of the modal, close it
+		  window.onclick = function(event) {
+		      if (event.target == modal) {
+		          modal.style.display = "none";
+		      }
+		  }
+
+
 		 
 
-		 $('.modal-header h2').html(String(d3.select(this).data().map(function(d){return d.Device})))
+		$('.modal-header h2').html(String(d3.select(this).data().map(function(d){return d.Device})))
 
 		
 
